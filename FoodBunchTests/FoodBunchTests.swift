ecd3f37e4a6fe3f2f03914cf99c5708f475177b6
furkan icoz <f_icoz@hotmail.com>
2025-04-18 20:51:42 +0300

    Initial Commit

---
+++ FoodBunchTests/FoodBunchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodBunchTests.swift
+//  FoodBunchTests
+//
+//  Created by Furkan ic on 18.04.2025.
+//
+
+import Testing
+@testable import FoodBunch
+
+struct FoodBunchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
